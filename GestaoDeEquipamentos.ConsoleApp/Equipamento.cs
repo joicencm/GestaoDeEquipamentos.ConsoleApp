23459667e4df29791b56463eaf0d7cd3c5d64911
joicencm <joicencm@gmail.com>
2025-05-05 21:17:10 -0300

    Estrutura POO

---
+++ GestaoDeEquipamentos.ConsoleApp/Equipamento.cs
@@ -0,0 +1,12 @@
+﻿namespace GestaoDeEquipamentos.ConsoleApp;
+
+
+        public class Equipamento
+        {
+            public int id;
+            public string nome;
+            public decimal precoAquisicao;
+            public string numeroSerie;
+            public string fabricante;
+            public DateTime datafabricante;
+        }
