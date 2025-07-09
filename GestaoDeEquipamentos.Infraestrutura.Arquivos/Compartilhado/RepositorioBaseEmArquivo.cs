8a59b0e722980562bca0e19c2d72fc453c1266d4
joicenas <joicenas22@gmail.com>
2025-07-09 09:17:30 -0300

    Implementada RepositorioBaseEmArquivo

---
+++ GestaoDeEquipamentos.Infraestrutura.Arquivos/Compartilhado/RepositorioBaseEmArquivo.cs
@@ -0,0 +1,71 @@
+﻿using GestaoDeEquipamentos.Dominio.Compartilhado;
+using GestaoDeEquipamentos.Infraestrutura.Arquivos.Compartilhado;
+
+namespace GestaoDeEquipamentos.Infraestrutura.Compartilhado;
+
+public abstract class RepositorioBaseEmArquivo<Tipo> where Tipo : EntidadeBase<Tipo>
+{
+    protected List<Tipo> registros = new List<Tipo>();
+    protected static int contadorIds = 0;
+
+    protected ContextoDados contexto;
+
+    protected RepositorioBaseEmArquivo(ContextoDados contexto)
+    {
+        this.contexto = contexto;
+        this.registros = ObterRegistros();
+    }
+
+    public void CadastrarRegistro(Tipo novoRegistro)
+    {
+        novoRegistro.id = ++contadorIds; ;
+
+        registros.Add(novoRegistro);
+
+        contexto.Salvar();
+    }
+
+    public bool EditarRegistro(int idSelecionado, Tipo registroAtualizado)
+    {
+        Tipo registroSelecionado = SelecionarRegistroPorId(idSelecionado);
+
+        if (registroSelecionado == null)
+            return false;
+
+        registroSelecionado.AtualizarRegistro(registroAtualizado);
+
+        return true;
+    }
+
+    public bool ExcluirRegistro(int idSelecionado)
+    {
+        Tipo registroSelecionado = SelecionarRegistroPorId(idSelecionado);
+
+        {
+            if (registroSelecionado is null)
+                return false;
+
+            registros.Remove(registroSelecionado);
+
+            return true;
+        }
+    }
+
+    public List<Tipo> SelecionarRegistros()
+    {
+        return registros;
+    }
+
+    public Tipo SelecionarRegistroPorId(int idSelecionado)
+    {
+        foreach (Tipo registro in registros)
+        {
+            if (registro.id.Equals(idSelecionado))
+                return registro;
+        }
+
+        return null;
+    }
+
+    protected abstract List<Tipo> ObterRegistros();
+}
